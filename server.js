13f1d5af4baf76b53bc0f2068076081ece40f794
Nathaniel Tucker <diicky@gmail.com>
2020-06-10 15:10:36 -0700

    update origin

--- server.js
+++ server.js
@@ -11,7 +11,11 @@ const test = process.env.MONGODB_URI_TEST;
 app.use(cors());
 app.use(express.json());
 
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 mongoose.connect(test, 
     {
         useNewUrlParser: true, 
