07bfdd6ff6da67f2c0e1ed2e6dafc9e3bb20679f
Nathaniel Tucker <diicky@gmail.com>
2020-06-13 11:50:01 -0700

    .

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localsonly.xyz");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
