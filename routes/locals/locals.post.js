e74c11ae039c04987df7d7885f5c3b6cb64195f2
Nathaniel Tucker <diicky@gmail.com>
2020-06-20 13:38:54 -0700

    add state abbreviations to search

--- routes/locals/locals.post.js
+++ routes/locals/locals.post.js
@@ -1,6 +1,22 @@
 let router = require('express').Router();
 let Local = require('../../models/locals.model');
-
+let abbrs = ["AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS",
+             "KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY",
+             "NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV",
+             "WI","WY","DC"]
+let states = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware",
+                 "Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana",
+                 "Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana",
+                 "Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota",
+                 "Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee",
+                 "Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming","District of Columbia"  
+                ]
+let getAbbr = (state)=>{
+    for (let i = 0; i < states.length; i++){
+        if(state == states[i])
+            return abbrs[i].toLowerCase();
+    }
+}
 router.route('/').post((req, res)=>{
 
     const name = req.body.name;
@@ -10,7 +26,12 @@ router.route('/').post((req, res)=>{
     const comments = req.body.comments;
     const hashtags = [name, ...req.body.hashtags];
     const rating = req.body.rating;
-    const addressTags = [address.city.toLowerCase(), address.state.toLowerCase(), address.zip];
+    const addressTags = [
+                            address.city.toLowerCase(), 
+                            address.state.toLowerCase(), 
+                            address.zip, 
+                            getAbbr(address.state.toLowerCase())
+                        ];
     const searchTags = [];
     hashtags.forEach(tag=>{
         var retag = tag.split(" ");
