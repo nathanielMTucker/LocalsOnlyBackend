6cd299d46631410b3f2d10e4ef7e3b2e7ec5258f
Gabe <finkegabriel@gmail.com>
2020-07-19 17:35:54 -0700

    added s3 keys

--- config.js
+++ config.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 const port = process.env.PORT || 5000;
 const uri = process.env.MONGODB_URI;
 const test = process.env.MONGODB_URI_TEST;
+const AWS_ACCESS_KEY = "AKIAIXNXV3UNV63Q6N4Q"
+const AWS_SECRET_KEY = "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"
+const AWS_BUCKET = "localsonlyimages"
 
 module.exports = {
     port,
@@ -19,5 +22,8 @@ module.exports = {
     },
     production: {
         connection: 'mongodb+srv://nmtucker2:UwXFICD6wjfBxJfb@cluster0-mgpfh.mongodb.net/test?retryWrites=true&w=majority',
-    }
+    },
+    AWS_ACCESS_KEY,
+    AWS_SECRET_KEY,
+    AWS_BUCKET
 }
