f96e771fb1765fc6ef763d8f228012333eab0a01
Nathaniel Tucker <diicky@gmail.com>
2020-07-27 20:55:35 -0700

    create user get request

--- routes/user/user.get.js
+++ routes/user/user.get.js
@@ -1,5 +1,11 @@
 let router = require('express').Router();
-let Local = require('../../models/locals.model');
+let User = require('../../models/locals.model');
 
-
+router.route('/:id').get((req, res)=>{
+    User.findOne({authID:req.params.id}).then(user=>{
+        res.json(user);
+    }).catch(err=>{
+        error(err);
+    })
+})
 module.exports = router;
